8501371139b4e85c75594551b326e0e14c00a123
jhonatan <jhon2226g@gmail.com>
2024-06-14 12:46:02 -0500

    todas las DC

--- scriptTablas.sql
+++ scriptTablas.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `bdinnovaschool` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bdinnovaschool`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: bdinnovaschool
@@ -25,14 +23,23 @@ DROP TABLE IF EXISTS `alumno`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `alumno` (
-  `codAlumno` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(20) NOT NULL,
-  `correo` varchar(50) NOT NULL,
-  `contraseña` varchar(45) NOT NULL,
-  PRIMARY KEY (`codAlumno`),
-  UNIQUE KEY `codAlumno_UNIQUE` (`codAlumno`)
+  `NombresApellidos` varchar(90) NOT NULL,
+  `estado` varchar(20) NOT NULL,
+  `Usuario_CodUsuario` int NOT NULL,
+  PRIMARY KEY (`Usuario_CodUsuario`),
+  KEY `fk_Alumno_Usuario1_idx` (`Usuario_CodUsuario`),
+  CONSTRAINT `fk_Alumno_Usuario1` FOREIGN KEY (`Usuario_CodUsuario`) REFERENCES `usuario` (`CodUsuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumno`
+--
+
+LOCK TABLES `alumno` WRITE;
+/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `material`
@@ -52,6 +59,15 @@ CREATE TABLE `material` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `material`
+--
+
+LOCK TABLES `material` WRITE;
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `penalizacion`
 --
 
@@ -60,9 +76,8 @@ DROP TABLE IF EXISTS `penalizacion`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `penalizacion` (
   `idPenalizacion` int NOT NULL AUTO_INCREMENT,
-  `fechaPenalizacion` datetime DEFAULT NULL,
+  `fechaPenalizacion` datetime NOT NULL,
   `descripcion` varchar(255) NOT NULL,
-  `estado` varchar(45) DEFAULT NULL,
   `Prestamo_idPrestamo` int NOT NULL,
   PRIMARY KEY (`idPenalizacion`),
   UNIQUE KEY `idPenalizacion_UNIQUE` (`idPenalizacion`),
@@ -70,6 +85,15 @@ CREATE TABLE `penalizacion` (
   CONSTRAINT `fk_Penalizacion_Prestamo1` FOREIGN KEY (`Prestamo_idPrestamo`) REFERENCES `prestamo` (`idPrestamo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `penalizacion`
+--
+
+LOCK TABLES `penalizacion` WRITE;
+/*!40000 ALTER TABLE `penalizacion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `penalizacion` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `prestamo`
@@ -82,13 +106,23 @@ CREATE TABLE `prestamo` (
   `idPrestamo` int NOT NULL AUTO_INCREMENT,
   `fechaPrestamo` datetime NOT NULL,
   `Solicitud_idSolicitud` int NOT NULL,
-  `estado` varchar(15) DEFAULT NULL,
+  `estado` varchar(15) NOT NULL,
+  `FechaDevReal` datetime DEFAULT NULL,
   PRIMARY KEY (`idPrestamo`),
   UNIQUE KEY `idPrestamo_UNIQUE` (`idPrestamo`),
   KEY `fk_Prestamo_Solicitud1_idx` (`Solicitud_idSolicitud`),
   CONSTRAINT `fk_Prestamo_Solicitud1` FOREIGN KEY (`Solicitud_idSolicitud`) REFERENCES `solicitud` (`idSolicitud`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COMMENT='							';
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prestamo`
+--
+
+LOCK TABLES `prestamo` WRITE;
+/*!40000 ALTER TABLE `prestamo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prestamo` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `solicitud`
@@ -100,17 +134,50 @@ DROP TABLE IF EXISTS `solicitud`;
 CREATE TABLE `solicitud` (
   `idSolicitud` int NOT NULL AUTO_INCREMENT,
   `cantidad` int NOT NULL,
-  `estado` varchar(15) DEFAULT NULL,
-  `Alumno_codAlumno` int NOT NULL,
+  `estado` varchar(15) NOT NULL,
   `Material_idMaterial` varchar(15) NOT NULL,
+  `Alumno_Usuario_CodUsuario` int NOT NULL,
   PRIMARY KEY (`idSolicitud`),
   UNIQUE KEY `idSolicitud_UNIQUE` (`idSolicitud`),
-  KEY `fk_Solicitud_Alumno1_idx` (`Alumno_codAlumno`),
   KEY `fk_Solicitud_Material1_idx` (`Material_idMaterial`),
-  CONSTRAINT `fk_Solicitud_Alumno1` FOREIGN KEY (`Alumno_codAlumno`) REFERENCES `alumno` (`codAlumno`),
+  KEY `fk_Solicitud_Alumno1_idx` (`Alumno_Usuario_CodUsuario`),
+  CONSTRAINT `fk_Solicitud_Alumno1` FOREIGN KEY (`Alumno_Usuario_CodUsuario`) REFERENCES `alumno` (`Usuario_CodUsuario`),
   CONSTRAINT `fk_Solicitud_Material1` FOREIGN KEY (`Material_idMaterial`) REFERENCES `material` (`idMaterial`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitud`
+--
+
+LOCK TABLES `solicitud` WRITE;
+/*!40000 ALTER TABLE `solicitud` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solicitud` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `CodUsuario` int NOT NULL AUTO_INCREMENT,
+  `Contrasenia` varchar(20) NOT NULL,
+  PRIMARY KEY (`CodUsuario`),
+  UNIQUE KEY `CodUsuario_UNIQUE` (`CodUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -121,4 +188,4 @@ CREATE TABLE `solicitud` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-10 23:43:58
+-- Dump completed on 2024-06-14 11:05:18
